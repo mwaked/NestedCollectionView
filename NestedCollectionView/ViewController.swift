146c6afd0dbcab6d7bed19e8b11cd71693ffa365
Mahmoud Waked <mwaked@gmail.com>
2021-01-22 22:10:35 +0300

    Initial Commit

---
+++ NestedCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NestedCollectionView
+//
+//  Created by Mahmoud Waked on 1/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
